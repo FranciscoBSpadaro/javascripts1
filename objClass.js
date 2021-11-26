d21e0c374e49754ace1af84fe22e3218f23e4a5a
Francisco Bianchi Spadaro <Francisco.Spadaro@educacao.sp.gov.br>
2021-11-26 16:45:17 -0300

    v1

---
+++ objClass.js
@@ -0,0 +1,21 @@
+// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Classes
+
+// Métodos Protótipos
+
+class Retangulo {
+    constructor(altura, largura) {
+      this.altura = altura; this.largura = largura;
+    }
+  //Getter
+    get area() {
+        return this.calculaArea()
+    }
+
+    calculaArea() {
+        return this.altura * this.largura;
+    }
+}
+
+const quadrado = new Retangulo(10, 10);
+
+console.log(quadrado.area);
