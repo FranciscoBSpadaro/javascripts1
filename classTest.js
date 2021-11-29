22a09ad4b805fa12f1e947ffeda62dfaea86d225
FranciscoBS89x <franciscobs89x@pop-os.localdomain>
2021-11-29 19:16:22 -0300

    versao atualizada

--- classTest.js
+++ classTest.js
@@ -1,30 +1,26 @@
 class User {
-  constructor(name, email) {
+  constructor(name, email,id) {
     this._name = name;
     this._email = email;
+    this._id = id;
   }
   // permite que seja inserindo valores na classe usando set
   set name(string) {
     this._name = string;
   }
-  set email(string) {
-    this._email = string;
-  }
   // permitir que seja consultado os valores inseridos na classe com get , get nao precisa de parametros
   get name() {
-    return this._name;
-  }
-  get email() {
-    return this._email;
+    return this._name
   }
 
+  // metodo para retorno das informaçoes para o console.log
   newUser() {
-    return `${this.name} , and email ${this._email} foi incluido na classe`;
+    return `${this.name} , and email ${this._email} foi incluido na classe com o id ${this._id}`;
   }
 }
 
-const user = new User("NickName", "nick@nicks.com.br");
-const user1 = new User("Mythoz89x", "mythoz@gmail.com");
+const user = new User("NickName", "nick@nicks.com.br", '1a');
+const user1 = new User("Mythoz89x", "mythoz@gmail.com", 2);
 
 console.log(user.newUser());
 console.log(user1.newUser());
