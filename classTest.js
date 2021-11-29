43fd6b6dab2a1a89433a7550484f71b6e8cf2663
Francisco Bianchi Spadaro <Francisco.Spadaro@educacao.sp.gov.br>
2021-11-29 16:24:57 -0300

    v1

---
+++ classTest.js
@@ -0,0 +1,34 @@
+class User {
+    constructor(name, email){
+       this._name = name
+       this._email= email
+    }
+// permite que seja inserindo valores na classe usando set
+    set name(string) {
+        this._name = string
+        
+
+    }
+    set email(string) {
+        this._email = string
+    }
+// permitir que seja consultado os valores inseridos na classe com get , get nao precisa de parametros
+    get name(){
+        return this._name
+    }
+    get email(){
+        return this._email
+    }
+
+    newUser() {
+        return `${this.name} , and email ${this._email} foi incluido na classe`
+    }
+    
+
+}
+
+var user = new User('NickName','nick@.com.br')
+var user1 = new User('Mythoz89x', "mythoz@gmail.com")
+
+console.log(user.newUser())
+console.log(user1.newUser())
